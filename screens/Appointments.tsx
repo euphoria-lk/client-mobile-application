0ce7f9198cf600c913d1c2a13f98f15713ff9c10
LahiruPathum <lahirupathum1223@gmail.com>
2020-07-04 19:31:59 +0530

    clean forum screen

--- screens/Appointments.tsx
+++ screens/Appointments.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { Button } from 'native-base';
+import { AuthContext } from '../navigation/cntext';
 
 interface AppointmentsProps {}
 
 const Appointments = (props: AppointmentsProps) => {
+  const {signOut} = React.useContext(AuthContext)
   return (
     <View style={styles.container}>
       <Text>Appointments</Text>
+      <View>
+        <Button onPress={()=>signOut()}>
+          <Text>Sign Out</Text>
+        </Button>
+      </View>
     </View>
   );
 };
